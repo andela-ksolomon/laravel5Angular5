0a445646051252d7ca53148c2ce44108044e2ab2
Solomon Kingsley <mkingzy1@gmail.com>
2018-02-20 01:17:29 +0100

    codebase setup

---
+++ server/resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Client</title>
+
+        <base href="/">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,400italic' rel='stylesheet' type='text/css'>
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+        {{--angular styles begin--}}
+		
+		{{--angular styles end--}}
+
+	</head>
+    <body>
+        <app-root></app-root>
+
+        {{--angular scripts begin--}}
+		<script type="text/javascript" src="inline.bundle.js"></script>
+		<script type="text/javascript" src="polyfills.bundle.js"></script>
+		<script type="text/javascript" src="styles.bundle.js"></script>
+		<script type="text/javascript" src="vendor.bundle.js"></script>
+		<script type="text/javascript" src="main.bundle.js"></script>
+		{{--angular scripts end--}}
+
+	</body>
+</html>
